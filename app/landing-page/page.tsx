60497cf054d41bef92898b8f3c4002064d19268f
drakkomaximo <andres.villanueva.tabares@gmail.com>
2025-07-05 15:14:01 -0500

    Landing page has been updated

--- app/landing-page/page.tsx
+++
@@ -1,9 +0,0 @@
-"use-client"
-import { KeyFeatures } from "@/components/landing-page/key-features"
-
-
-export default function LandingPage() {
-    return(
-        <KeyFeatures />
-    )
-}
--- pages/landing/index.tsx
+++ pages/landing/index.tsx
@@ -1,9 +1,11 @@
+import { KeyFeatures } from "@/components/landing-page/key-features";
 import Hero from "./hero";
 
 export default function LandingPage() {
   return (
     <main>
       <Hero />
+      <KeyFeatures />
     </main>
   );
 } 
