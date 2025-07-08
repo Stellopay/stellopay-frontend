5231c8cd6ed111e7a5932faeef92de14b9537ef3
Jagadeeshftw <jagadeesh26062002@gmail.com>
2025-07-08 18:04:51 +0530

    refactor: rename EmailInput.tsx to email-input.tsx (kebab-case) and update imports

---
+++ components/common/email-input.tsx
@@ -0,0 +1,35 @@
+import React, { ChangeEvent } from "react";
+import { EmailInputProps } from "@/types/ui";
+
+const EmailInput: React.FC<EmailInputProps> = ({ value, onChange }) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onChange(event.target.value);
+  };
+  return (
+      <div className="w-full">
+        <label
+          htmlFor="emailinput"
+          className="text-[0.875rem] font-medium block mb-1"
+        >
+          Email address
+        </label>
+        <div
+          className={`flex items-center border border-[#2D2D2D] rounded-[0.375rem]  h-[48px] md:h-[52px] overflow-hidden`}
+        >
+
+  
+          <input
+            type="text"
+            id="emailinput"
+            name="email"
+            placeholder="Mayasullivan@gmail.com"
+            value={value}
+            onChange={handleChange}
+            className={`px-3 w-full focus:outline-none text-[0.875rem] text-[#ffffff] `}
+          />
+        </div>
+      </div>
+    );
+};
+
+export default EmailInput;
