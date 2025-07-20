6492cfee9478d3f4bdc4637bd2a9f0a594d9744b
Jagadeeshftw <jagadeesh26062002@gmail.com>
2025-07-20 14:40:21 +0530

    feat: add HeroContent object with promotional text and image details for landing page

--- types/landing.ts
+++ types/landing.ts
@@ -3,4 +3,17 @@ export interface FeatureCardProps {
   imageSrc: string;
   title: string;
   description: string;
+}
+
+export const HeroContent = {
+    titleOne: "Secure Digital Payments",
+    titleTwo: "For a Smarter Future",
+    description: "Stellopay, automating salary payments globally, while experiencing instant, secure payroll on the stellar blockchain",
+    button: "Get started",
+    buttonSecondary: "Learn More",
+    image: "/landing/heroCards.png",
+    imageAlt: "Stellopay Cards",
+    imageWidth: 400,
+    imageHeight: 250,
+    imageClassName: "w-full h-auto shadow-xl",
 } 
