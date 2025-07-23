2d1f822c824d3893add8932c8b17d58092ddbf64
DevEmmac <eolasunkanmi@gmail.com>
2025-07-23 01:23:31 +0100

    added account summary

--- components/dashboard/dashboard-page.tsx
+++ components/dashboard/dashboard-page.tsx
@@ -1,9 +1,10 @@
+'user client'
 import React from 'react';
 import Image from "next/image";
 import Link from 'next/link';
-// import DashboardPayment from "@/components/dashboard/dashboard-header";
+import AccountSummary from '@/components/dashboard/account-summary';
 import { IoNotifications} from "react-icons/io5";
-import { TbSettings } from "react-icons/tb";
+import { TbSettings, TbDownload } from "react-icons/tb";
 import { RxQuestionMarkCircled } from "react-icons/rx";
 import { PiPaperPlaneTilt } from "react-icons/pi";
 
@@ -43,21 +44,25 @@ export default function dashboard() {
                     </div>
                 </div>
 
-                <div className='max-w-[1400px] h-[1250px] border-2 border-purple-600 py-6 px-10'>
-                    <div className="w-full px-4 md:px-6">
+                <div className='max-w-[1400px] h-[1250px] border-1 border-purple-600 py-6 px-10'>
+                    <div className="w-full">
                        <div className="max-w-screen-xl mx-auto flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4">
-                           <h1 className="text-white text-2xl font-semibold pl-4"></h1>
+                           <h1 className="text-white text-2xl font-semibold ml-0">Dashboard</h1>
                             <div className="flex flex-col sm:flex-row gap-2 sm:gap-3 w-full sm:w-auto">
                                 <button className="flex items-center justify-center gap-2 bg-black text-white px-4 py-2 rounded-md border border-[#2c2c2c] hover:bg-[#111] transition w-full sm:w-auto">
-                                    <span><PiPaperPlaneTilt /></span> Send Payment
+                                    <span><PiPaperPlaneTilt /></span>Send Payment
                                 </button>
                                 <button className="flex items-center justify-center gap-2 bg-white text-black px-4 py-2 rounded-md border hover:bg-[#f3f3f3] transition w-full sm:w-auto">
-                                   <span>⬇️</span> Request Payment
+                                   <span><TbDownload /></span> Request Payment
                                 </button>     
                             </div>    
                         </div> 
-                    </div>     
+                    </div>  
+
+                    <AccountSummary />  
                 </div>
+
+                
             </div> 
     </div>              
   )
