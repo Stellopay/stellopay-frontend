454048ef85f4af9c885c6488789ecff73fc68b3a
Jagadeeshftw <jagadeesh26062002@gmail.com>
2025-12-26 00:02:46 +0530

    fix: update app layout to support fixed sidebar positioning

--- components/common/app-layout.tsx
+++ components/common/app-layout.tsx
@@ -10,24 +10,30 @@ export default function AppLayout({ children }: AppLayoutProps) {
   const { isSidebarOpen, isMobile } = useSidebar();
 
   return (
-    <div className="relative   min-h-screen">
+    <div className="relative min-h-screen">
+      {/* Sidebar for desktop - fixed position */}
+      {!isMobile && (
+        <div
+          className={`fixed left-0 top-0 h-screen z-30 transition-all duration-300 ease-in-out ${
+            isSidebarOpen ? "w-[16.25rem]" : "w-[6rem]"
+          }`}
+        >
+          <SideBar />
+        </div>
+      )}
+
+      {/* Main content area - with margin to account for sidebar */}
       <div
-        className={`grid transition-all duration-300 ease-in-out ${
+        className={`relative min-h-screen transition-all duration-300 ease-in-out ${
           isMobile
-            ? "grid-cols-1"
+            ? "ml-0"
             : isSidebarOpen
-              ? "grid-cols-[16.25rem_1fr]"
-              : "grid-cols-[6rem_1fr]"
+              ? "ml-[16.25rem]"
+              : "ml-[6rem]"
         }`}
       >
-        {/* Sidebar for desktop */}
-        {!isMobile && <SideBar />}
-
-        {/* Main content area */}
-        <div className=" relative  min-h-screen">
-          <Navbar />
-          <main className="flex flex-col">{children}</main>
-        </div>
+        <Navbar />
+        <main className="flex flex-col">{children}</main>
       </div>
 
       {/* Mobile fullscreen sidebar */}
